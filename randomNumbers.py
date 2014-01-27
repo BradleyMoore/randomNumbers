5e2e7959c934aa3e33439ed353e910c66fcac00c
Bradley Moore <BradleyMooreTX@gmail.com>
2014-01-27 10:31:05 -0700

    add code

---
+++ randomNumbers.py
@@ -0,0 +1,9 @@
+import random
+
+bottom = int(raw_input('\nWhat is the bottom number in your range?\n> '))
+top = int(raw_input('\nWhat is the top number in your range?\n> '))
+
+while __name__ == '__main__':
+	print '\n' * 25
+	print random.randint(bottom, top)
+	raw_input('\nPress Enter for more another.')
