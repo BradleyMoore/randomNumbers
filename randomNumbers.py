de68f5b400019e063cf72c33a04e2b0bdab30e99
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-29 17:26:50 +0000

    remove stupid typo

--- randomNumbers.py
+++ randomNumbers.py
@@ -6,4 +6,4 @@ top = int(raw_input('\nWhat is the top number in your range?\n> '))
 while __name__ == '__main__':
 	print '\n' * 25
 	print random.randint(bottom, top)
-	raw_input('\nPress Enter for more another.')
+	raw_input('\nPress Enter for another.')
